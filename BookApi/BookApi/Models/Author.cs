aee25def40f5a038f1852eeacabc58220c5374bd
hboutaleb <boutalebhocine@yahoo.fr>
2019-02-11 16:58:56 +0100

    ajout de projet BookApi

---
+++ BookApi/BookApi/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookApi.Models
+{
+    public class Author
+    {
+       
+            public int Id { get; set; }
+
+            [Required]
+            public string Firstname { get; set; }
+
+            [Required]
+            public string Lastname { get; set; }
+        
+    }
+}
